e89c0b5609b8f40b54e6e24d4dc3e696e7479d8c
mh-shovon <shovon33266@gmail.com>
2025-09-23 00:03:38 +0600

    Class-2 :: Add multiple Selenium test classes for login and link testing and add NavigationTest for test the navigation in selenium

---
+++ src/test/java/Selenium/Class/Class_02/LintTest.java
@@ -0,0 +1,27 @@
+package Selenium.Class.Class_02;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class LintTest {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://parabank.parasoft.com/parabank/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+
+        System.out.println("Current URL: " + driver.getCurrentUrl());
+        System.out.println("Page Title: " + driver.getTitle());
+
+        driver.findElement(By.linkText("Register")).click();
+        Thread.sleep(2000);
+
+        driver.findElement(By.partialLinkText("Login info?")).click();
+        Thread.sleep(2000);
+
+        driver.quit();
+    }
+}
